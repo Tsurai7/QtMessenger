3ce240dfd8c300ded7564287e24692d939a8e8aa
Nikita Kharashun <tsurai7walkin@gmail.com>
2023-11-25 12:58:11 +0300

    Added server implementaion

--- Server/server.cpp
+++ Server/server.cpp
@@ -2,5 +2,45 @@
 
 Server::Server()
 {
+    if(this->listen(QHostAddress::Any, 8080))
+    {
+        qDebug() << "Server started";
+    }
+    else
+    {
+        qDebug() << "Error occured while starting the server";
+    }
+}
 
+void Server::incomingConnection(qintptr socketDescriptor)
+{
+    socket = new QTcpSocket;
+
+    socket->setSocketDescriptor(socketDescriptor);
+
+    connect(socket, &QTcpSocket::readyRead, this, &Server::onReadyRead);
+    connect(socket, &QTcpSocket::disconnected, this, &Server::deleteLater);
+
+    Sockets.push_back(socket);
+    qDebug() << "Client connected";
+}
+
+void Server::onReadyRead()
+{
+    socket = (QTcpSocket*)sender();
+
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_6_6);
+
+    if(in.status() == QDataStream::Ok)
+    {
+        qDebug() << "Reading...";
+        QString inputStr;
+        in >> inputStr;
+        qDebug() << inputStr;
+    }
+    else
+    {
+        qDebug() << "Error while reading messages";
+    }
 }
--- Server/server.h
+++ Server/server.h
@@ -1,11 +1,24 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <QTcpServer>
+#include <QTcpSocket>
 
-class Server
+class Server : public QTcpServer
 {
+    Q_OBJECT
 public:
     Server();
+    QTcpSocket *socket;
+
+private:
+    QVector<QTcpSocket*> Sockets;
+    QByteArray Data;
+    void SendToClient();
+
+public slots:
+    void incomingConnection(qintptr socketDescriptor);
+    void onReadyRead();
 };
 
 #endif // SERVER_H
